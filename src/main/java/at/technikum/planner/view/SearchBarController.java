230af3715ab0ca67fc574ba9322085bf60e4f6c7
Siyar Yamin <yaminsiyar@yahoo.de>
2023-06-26 12:07:07 +0200

    SearchBar added

---
+++ src/main/java/at/technikum/planner/view/SearchBarController.java
@@ -0,0 +1,14 @@
+package at.technikum.planner.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SearchBarController {
+    @FXML public Button searchButton;
+    @FXML public TextField searchTextField;
+
+    public void onSearchButton(ActionEvent actionEvent) {
+    }
+}
