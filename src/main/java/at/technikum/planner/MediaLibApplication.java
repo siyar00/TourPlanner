a08bb142c2f0841b596afb63e1d2d67a6bbec443
Siyar Yamin <yaminsiyar@yahoo.de>
2023-06-26 13:56:15 +0200

    Added some Controller, FXML data and others

--- src/main/java/at/technikum/planner/MediaLibApplication.java
+++ src/main/java/at/technikum/planner/MediaLibApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -22,7 +23,8 @@ public class MediaLibApplication extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/MainWindow.fxml")),
                 ResourceBundle.getBundle("at.technikum.planner.view.gui_strings_de"), new JavaFXBuilderFactory());
-        stage.setTitle("Media Planner");
+        stage.setTitle("Dora the Explorer");
+        stage.getIcons().add(new Image(Objects.requireNonNull(MediaLibApplication.class.getResourceAsStream("images/dora.png"))));
         stage.setScene(new Scene(root));
         stage.show();
     }
---
+++ src/main/java/at/technikum/planner/view/TourListController.java
@@ -0,0 +1,11 @@
+package at.technikum.planner.view;
+
+import javafx.event.ActionEvent;
+
+public class TourListController {
+    public void onButtonAdd(ActionEvent actionEvent) {
+    }
+
+    public void onButtonRemove(ActionEvent actionEvent) {
+    }
+}
