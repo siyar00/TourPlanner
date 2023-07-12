f5c48aa00f78beae3f045922913afb38e2b2ca36
bhinder-if21b107 <if21b107@technikum-wien.at>
2023-07-12 18:02:28 +0200

    ControllerFactoryTest added (parameterized tests)

---
+++ Application/src/test/java/at/technikum/planner/functionality/ControllerFactoryTest.java
@@ -0,0 +1,77 @@
+package at.technikum.planner.functionality;
+import at.technikum.planner.view.*;
+import at.technikum.planner.view.dialog.TourListDialogController;
+import at.technikum.planner.view.dialog.TourLogsDialogController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ControllerFactoryTest {
+    @Mock
+    private ConfigurableApplicationContext applicationContext;
+
+    @Mock
+    private ResourceBundle bundle;
+
+    private ControllerFactory factory;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        factory = new ControllerFactory(applicationContext, bundle);
+    }
+
+    @Test
+    void createMainWindowController() {
+        Object controller = factory.create(MainWindowController.class);
+        assertNotNull(controller);
+        assertTrue(controller instanceof MainWindowController);
+    }
+
+    @Test
+    void createRouteMapController() {
+        Object controller = factory.create(RouteMapController.class);
+        assertNotNull(controller);
+        assertTrue(controller instanceof RouteMapController);
+    }
+
+    @Test
+    void createSearchBarController() {
+        Object controller = factory.create(SearchBarController.class);
+        assertNotNull(controller);
+        assertTrue(controller instanceof SearchBarController);
+    }
+
+    @Test
+    void createTourLogsController() {
+        Object controller = factory.create(TourLogsController.class);
+        assertNotNull(controller);
+        assertTrue(controller instanceof TourLogsController);
+    }
+
+    @Test
+    void createTourListDialogController() {
+        Object controller = factory.create(TourListDialogController.class);
+        assertNotNull(controller);
+        assertTrue(controller instanceof TourListDialogController);
+    }
+
+    @Test
+    void createTourLogsDialogController() {
+        Object controller = factory.create(TourLogsDialogController.class);
+        assertNotNull(controller);
+        assertTrue(controller instanceof TourLogsDialogController);
+    }
+
+    @Test
+    void createUnknownController() {
+        assertThrows(IllegalArgumentException.class, () -> factory.create(Object.class));
+    }
+}
